1d8c248e3b2e91d1a0c4600741d7ab9603199a85
Gaelan Steele <gbs@canishe.com>
2016-12-05 20:35:56 -0800

    Make deterministic drama embeddable, hopefully.

--- web.rb
+++ web.rb
@@ -4,25 +4,33 @@ require './draminate'
 get '/' do
 	seed = Random.new_seed
 	Random.srand(seed)
-	erb :drama, locals: {seed: seed}
+	erb :drama, locals: {seed: seed, drama: draminate}
 end
 
 get '/:seed' do
 	seed = params[:seed].to_i
 	Random.srand(seed)
-	erb :drama, locals: {seed: seed}
+	erb :drama, locals: {seed: seed, drama: draminate}
 end
 
 __END__
 
 @@drama
+<!doctype html>
+<html>
+<head>
+<meta name='description' content='<%= drama %>'>
+</head>
+<body>
 <style>
 * {
 	text-align: center;
 }
 .drama { font-size: 32px; }
 </style>
-<div class='drama'><%= draminate %></div>
+<div class='drama'><%= drama %></div>
 <a href='/'>Get more drama!</a> <br>
 <a href='/<%= seed %>'>Permalink</a> <br>
 <a href='https://github.com/Gaelan/MCDrama'>GitHub
+</body>
+</html>
