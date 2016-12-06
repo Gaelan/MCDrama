084a749485dab21202de70083e6e7ded785f6b0d
Gaelan Steele <gbs@canishe.com>
2016-12-05 22:24:53 -0800

    Add reactions.

--- draminate.rb
+++ draminate.rb
@@ -1,22 +1,33 @@
+class MissingData < StandardError; end
+
 def select_from_dict(dict, item)
-	Hash[File.read("data/#{dict}").split("\n").map { |x| x.split ":" }][item] || "<<ERROR: no #{dict} for #{item}>>"
+	throw MissingData.new unless item
+	hash = Hash[File.read("data/#{dict}").split("\n").map { |x| x.split ":" }]
+	throw MissingData.new unless hash[item]
+	hash[item]
 end
 
 def select_from_file(name, selections = {})
 	File.read("data/#{name}").split("\n").sample
-		.gsub(/\%([a-z]+)/) do
-			type = $1
-			value = select_from_file type, selections
-			selections[type] = value unless selections[type]
-			value
-		end
-		.gsub(/\$([a-z]+):([a-z]+)/) do
+	.gsub(/\%([a-z]+)/) do
+		type = $1
+		value = select_from_file type, selections
+		selections[type] = value unless selections[type]
+		value
+	end
+end
+
+def draminate
+	begin
+		selections = {}
+		drama = select_from_file 'root', selections
+		drama.gsub(/\$([a-z]+):([a-z]+)/) do
 			source_type = $1
 			attr = $2
 			select_from_dict(attr, selections[source_type])
 		end
-end
-
-def draminate
-	select_from_file 'patterns'
+	rescue
+		puts 'retrying'
+		retry
+	end
 end
